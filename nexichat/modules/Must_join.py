82ad59cfa866e91af4f941ead6960718e4cd33f1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-23 01:07:45 +0530

    Update Must_join.py

--- nexichat/modules/Must_join.py
+++ nexichat/modules/Must_join.py
@@ -34,7 +34,7 @@ async def must_join_channel(app: Client, msg: Message):
                     await msg.stop_propagation()
                 except ChatWriteForbidden:
                     pass
-            except PeerIdInvalid:
-                pass
+    except PeerIdInvalid:
+        pass
     except ChatAdminRequired:
         print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
