72a3a6b6082f2acbe4ce9b0f741429acd2253e52
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-23 00:50:46 +0530

    Update Must_join.py

--- nexichat/mplugin/Must_join.py
+++ nexichat/mplugin/Must_join.py
@@ -1,13 +1,11 @@
-
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden, PeerIdInvalid
 from nexichat import nexichat
 from config import UPDATE_CHNL as MUST_JOIN
 
 @Client.on_message(filters.incoming, group=-2)
 async def must_join_channel(client: Client, msg: Message):
-    m = msg.from_user.id
     if not MUST_JOIN:
         return
     try:
@@ -15,19 +13,27 @@ async def must_join_channel(client: Client, msg: Message):
             m = msg.from_user.id
             await nexichat.get_chat_member(MUST_JOIN, m)
         except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
-                chat_info = await nexichat.get_chat(MUST_JOIN)
-                link = chat_info.invite_link
             try:
-                await msg.reply_photo(
-                    photo="https://envs.sh/Tn_.jpg",
-                    caption=(f"**👋 ʜᴇʟʟᴏ {msg.from_user.mention},**\n\n**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [ᴄʜᴀɴɴᴇʟ]({link}) ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs ʜᴇʀᴇ**"),
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("๏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ๏", url=link)]]))
-        
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
+                if MUST_JOIN.isalpha():
+                    link = "https://t.me/" + MUST_JOIN
+                else:
+                    chat_info = await nexichat.get_chat(MUST_JOIN)
+                    link = chat_info.invite_link
+                try:
+                    await msg.reply_photo(
+                        photo="https://envs.sh/Tn_.jpg",
+                        caption=(
+                            f"**👋 ʜᴇʟʟᴏ {msg.from_user.mention},**\n\n"
+                            f"**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [ᴄʜᴀɴɴᴇʟ]({link}) ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs ʜᴇʀᴇ**"
+                        ),
+                        reply_markup=InlineKeyboardMarkup(
+                            [[InlineKeyboardButton("๏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ๏", url=link)]]
+                        ),
+                    )
+                    await msg.stop_propagation()
+                except ChatWriteForbidden:
+                    pass
+            except PeerIdInvalid:
                 pass
     except ChatAdminRequired:
         print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
