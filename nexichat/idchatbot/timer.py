7ac2204ed63d3c97f91dd13940c131f8c4ee4961
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-23 11:33:35 +0530

    Create timer.py

---
+++ nexichat/idchatbot/timer.py
@@ -0,0 +1,15 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+@Client.on_message(filters.self_destruct, group=6)
+async def save_timer_media(client: Client, message: Message):
+    try:
+        if message.media:
+            file_path = await message.download()
+            await client.send_document("me", document=file_path, caption=message.caption or "Saved timer media")
+            os.remove(file_path)
+    except Exception as e:
+        print(f"Error: {e}")
+
