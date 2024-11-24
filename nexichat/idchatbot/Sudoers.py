829c899754549b2e2163a05fbe08a483a39b44c7
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-24 17:41:01 +0530

    Update Sudoers.py

--- nexichat/idchatbot/Sudoers.py
+++ nexichat/idchatbot/Sudoers.py
@@ -126,7 +126,7 @@ async def edit_or_reply(msg: Message, **kwargs):
 @Client.on_edited_message(
     filters.command("sh") & filters.user(OWNER_ID) & ~filters.forwarded & ~filters.via_bot
 )
-@Client5.on_message(filters.command("sh") & filters.user(OWNER_ID) & ~filters.forwarded & ~filters.via_bot)
+@Client.on_message(filters.command("sh") & filters.user(OWNER_ID) & ~filters.forwarded & ~filters.via_bot)
 async def shellrunner(_, message: Message):
     if len(message.command) < 2:
         return await edit_or_reply(message, text="<b>ᴇxᴀᴍᴩʟᴇ :</b>\n/sh git pull")
